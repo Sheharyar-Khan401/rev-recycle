8683e1a9dc06997a38382cb87087dddb45e6ed01
Sheharyar Khan <98257648+Sheharyar-Khan401@users.noreply.github.com>
2024-05-31 14:05:14 +0500

    Add files via upload
    intial commit

---
+++ typings.d.ts
@@ -0,0 +1,13 @@
+declare module "*.module.css";
+declare module "*.svg" {
+  import React = require('react')
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
+
+}
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.jpg";
+declare module "*.mp3";
